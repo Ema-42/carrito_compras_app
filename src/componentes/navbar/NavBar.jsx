84fe6ec0e7c6779141bc36c146f9cf58731e3874
Ema-42 <emanuelchusgp@gmail.com>
2024-07-19 22:08:38 -0400

    feat: modo oscuro y correccion de diseo responsive

--- src/componentes/navbar/NavBar.jsx
+++ src/componentes/navbar/NavBar.jsx
@@ -1,43 +1,55 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { Badge } from "@mui/material";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { CarritoContext } from "../../context/CarritoContext";
 import "./style.css";
 const NavBar = () => {
+  const [tema, setTema] = useState("light");
+  const cambiarTema = (eleccion) => {
+    setTema(eleccion);
+    console.log(tema);
+  };
   const { listaCompras } = useContext(CarritoContext);
   return (
     <>
       <nav
-        className="navbar   navbar-expand-lg bg-body-tertiary"
-        id="navbar-pagina"
+        className={`navbar ${
+          tema === "dark" ? "tema-dark" : "bg-body-tertiary"
+        }`}
       >
-        <div className="container-fluid">
-          <NavLink to={"/"} className="navbar-brand" href="#">
-            Carrito
+        <div className="container-fluid d-flex justify-content-start ">
+          <NavLink to="/" className="navbar-brand navbar-link">
+            <img
+              src="src/assets/logo.png"
+              alt="Logo"
+              width="35"
+              height="30"
+              className="d-inline-block align-text-top"
+            />{" "}
+            <span
+              className={`titulo-app ${tema === "dark" && "tema-dark-titulo"}`}
+            >
+              Carrito de Compras
+            </span>
           </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink to={"/carrito"}>
-                  <Badge badgeContent={listaCompras.length} color="secondary">
-                    <ShoppingCart color="action" />
-                  </Badge>
-                </NavLink>
-              </li>
-            </ul>
+          <div className="color-tema">
+            {tema === "dark" ? (
+              <a href="#" onClick={() => cambiarTema("ligth")}>
+                <img src="src/assets/ligth.png" className="icono-ligth"></img>
+              </a>
+            ) : (
+              <a href="#" onClick={() => cambiarTema("dark")}>
+                <img src="src/assets/dark.png" className="icono-dark"></img>
+              </a>
+            )}
           </div>
+          <NavLink to="/carrito" className="navbar-link "></NavLink>
+          <NavLink to="/carrito" className="navbar-link carrito">
+            <Badge badgeContent={listaCompras.length} color="secondary">
+              <ShoppingCart color="action" />
+            </Badge>
+          </NavLink>
         </div>
       </nav>
     </>
--- src/pages/CarritoPages.jsx
+++ src/pages/CarritoPages.jsx
@@ -2,12 +2,8 @@ import React, { useContext } from "react";
 import { CarritoContext } from "../context/CarritoContext";
 
 const CarritoPages = () => {
-  const {
-    listaCompras,
-    aumentarCantidad,
-    disminuiirCantidad,
-    eliminarCompra,
-  } = useContext(CarritoContext);
+  const { listaCompras, aumentarCantidad, disminuiirCantidad, eliminarCompra } =
+    useContext(CarritoContext);
   const calcularTotal = () => {
     return listaCompras
       .reduce((total, item) => total + item.price * item.cantidad, 0)
@@ -18,60 +14,79 @@ const CarritoPages = () => {
   };
   return (
     <>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Item N°</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Accion</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listaCompras.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td>{item.price}</td>
-              <td>
-                <div className="btn-group">
-                  <button
-                    className="btn btn-outline-primary"
-                    onClick={() => disminuiirCantidad(item.id)}
-                  >
-                    -
-                  </button>
-                  <button className="btn btn-primary">{item.cantidad}</button>
-                  <button
-                    className="btn btn-outline-primary"
-                    onClick={() => aumentarCantidad(item.id)}
-                  >
-                    +
-                  </button>
-                </div>
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => eliminarCompra(item.id)}
-                >
-                  Eliminar
-                </button>
-              </td>
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Item N°</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Cantidad</th>
+              <th scope="col">Accion</th>
             </tr>
-          ))}
-          <th>
-            <b>Total</b>
-          </th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>{calcularTotal()} Bs.</td>
-        </tbody>
-      </table>
-      <div className="d-grid gap-2 mt-4" >
-        <button className="btn btn-primary "onClick={handleImpresion} disabled={listaCompras.length<1}>COMPRAR</button>
+          </thead>
+          <tbody>
+            {listaCompras.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.price}</td>
+                <td>
+                  <div className="btn-group">
+                    <button
+                      className="btn btn-outline-primary"
+                      onClick={() => disminuiirCantidad(item.id)}
+                    >
+                      -
+                    </button>
+                    <button className="btn btn-primary">{item.cantidad}</button>
+                    <button
+                      className="btn btn-outline-primary"
+                      onClick={() => aumentarCantidad(item.id)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => eliminarCompra(item.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-trash-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"></path>
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+            <tr>
+              <th>
+                <b>Total</b>
+              </th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>{calcularTotal()} Bs.</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="d-grid gap-2 mt-4">
+        <button
+          className="btn btn-primary"
+          onClick={handleImpresion}
+          disabled={listaCompras.length < 1}
+        >
+          COMPRAR
+        </button>
       </div>
     </>
   );

160d6c6a5fc49336c523f6920a316dd05fe9c23e
Ema-42 <emanuelchusgp@gmail.com>
2024-08-01 15:58:32 -0400

    fix: diseno

--- src/Carrito/CarritoApp.jsx
+++ src/Carrito/CarritoApp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NavBar from "../componentes/navbar/NavBar";
 import ComprasPages from "../pages/ComprasPages";
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import CarritoApp from "./Carrito/CarritoApp.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
       <CarritoApp></CarritoApp>
     </React.StrictMode>
-    
   </BrowserRouter>
 );
